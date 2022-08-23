a467b641bbec734b2a24ec7e85092269e83ff6ab
Geovanemotta <gamesstarbrasil@gmail.com>
2022-08-23 18:43:58 -0300

    Add a pagina 2

---
+++ script.js
@@ -0,0 +1,6 @@
+let titulo = document.getElementById ("campo1")
+let descricao = document.getElementById ("campo2")
+let url = document.getElementById ("campo3")
+let outrocamp = document.getElementById ("campo4")
+let morecamp = document.getElementById ("campo5")
+
