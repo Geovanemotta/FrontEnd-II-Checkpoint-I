3fdfb33d4c71eda565522eadf3f9d353a240d4ca
Geovanemotta <gamesstarbrasil@gmail.com>
2022-08-23 22:35:31 -0300

    atualizacao site

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let titulo = document.getElementById ("campo1")
-let descricao = document.getElementById ("campo2")
-let url = document.getElementById ("campo3")
-let outrocamp = document.getElementById ("campo4")
-let morecamp = document.getElementById ("campo5")
+let form = document.querySelector("form")
 
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    console.log("form enviado");
+})
