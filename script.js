fd77868cb18ffc37f8c274f48cf7fd70da81bda0
Geovanemotta <gamesstarbrasil@gmail.com>
2022-08-29 23:12:40 -0300

    Atualizacao de responsividade

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-
-   const tituloCard = document.getElementById("titleForm")
-   const urlCard = document.getElementById("urlForm")
-   const descricaoCard = document.getElementById("textForm")
-   const categoriaCard = document.getElementById("categoria")
-   const enviarCard = document.querySelector("#submit")
-
-   window.onload = function (){ 
+const tituloCard = document.getElementById("titleForm");
+const urlCard = document.getElementById("urlForm");
+const descricaoCard = document.getElementById("textForm");
+const categoriaCard = document.getElementById("categoria");
+const enviarCard = document.querySelector("#submit");
 
 
-   enviarCard.addEventListener('click', enviar => {  
-    
-            enviar.preventDefault()
-        
-            let addCard = document.createElement('div')
-            addCard.innerHTML = `
+
+function adicionarDadosNaPagina() {
+
+  enviarCard.addEventListener("click", (enviar) => {
+    enviar.preventDefault();
+
+    if (tituloCard.value === ""){
+        alert("Verifique os dados e tente novamente!!")
+        tituloCard.focus();
+        return false
+    }
+
+    let addCard = document.createElement("div");
+    addCard.innerHTML = `
             <div class="card">
                     <div>
                         <img class="background_card" src="${urlCard.value}" alt="">
@@ -22,106 +27,8 @@
                     <p class="description">${descricaoCard.value}</p>
                     <p class="categoriaCard">${categoriaCard.value}</p>
                 </div>
-            `
-            document.querySelector(".dadosGaleria").appendChild(addCard)
-    } )
-}   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // let form = document.querySelector("form")
-    // let inputs = document.getElementById("textarea")
-    // let divDadosGaleria = document.getElementById("dadosGaleria")
-
-
-// form.addEventListener("submit", (evento) => {
-//     evento.preventDefault()
-//     let titulo = form["title"].value
-//     let url = form["url"].value
-//     let descript = form["description"].value
-//     let category = form["categoria"].value
-//     adicionarDadosNaPagina()
-
-//     console.log(titulo, url, descript, category);
-// })
-
-  
-// function adiconarImagensNaGaleria(listaInputs) {
-//     for (const input of listaInputs) {
-//         let img = document.createElement("img")
-//         img.src = input.value
-//         divGaleria.appendChild(img)
-
-//         input.value = ""
-//     }
-// }
-    
-
-// function adicionarDadosNaPagina(inputDados) {
-//     for (const input of inputDados) {
-        
-        
-//         let urlImg = document.createElement("img")
-//         // let description = document.createElement("p")
-        
-        
-
-//         urlImg.src = input.value
-//         divDadosGaleria.appendChild(urlImg)
-
-//         // description.innerHTML = input.value
-//         // divDadosGaleria.appendChild(description)
-        
-
-//        input.value = ""
-//     }
-// }
-
-
-// function adicionarTitulo(inputDados) {
-//     for (const input of inputDados) {
-//         let title = document.createElement("h2")
-//         title.innerText = input.value
-//         divDadosGaleria.appendChild(title)
-
-//         input.value = ""
-//     }
-// }
-
-// function adicionarImg(inputDados) {
-//     for (const input of inputDados) {
-//         let urlImg = document.createElement("img")
-//         urlImg.src = input.value
-//         divDadosGaleria.appendChild(urlImg)
-
-//         input.value = ""
-//     }
-// }
-
-// function addDescription(inputDados) {
-//     for (const input of inputDados) {
-//         let description = document.createElement("p")
-//         description.innerHTML = input.value
-//         divDadosGaleria.appendChild(description)
-
-//         input.value = ""
-//     }
-// }
-
+            `;
+    document.querySelector(".dadosGaleria").appendChild(addCard);
+  });
+}
 
