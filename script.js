35f2589ebfa91b78d6cf10f34fa5f53ecb0c0626
Geovanemotta <gamesstarbrasil@gmail.com>
2022-08-31 19:34:29 -0300

    Atualização final checkpoint

--- script.js
+++ script.js
@@ -3,44 +3,50 @@ const urlCard = document.getElementById("urlForm");
 const descricaoCard = document.getElementById("textForm");
 const categoriaCard = document.getElementById("categoria");
 const enviarCard = document.querySelector("#submit");
-const tagError = document.getElementById("error")
+const form = document.querySelector("form")
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+
+let titulo = form["title"]
+    titulo.onblur = () => {     
+    titulo.value = titulo.value.toLocaleUpperCase()    
+    const tagError = titulo.parentElement.children[2]
+    if (titulo.value.length < 1 ) {        
+        tagError.innerHTML = "O campo não foi preenchido corretamente!!"
+    } else {
+        tagError.innerHTML = ""
+    }   
+}
+
+let url = form["url"]
+    url.onblur = () => {       
+    const tagError = url.parentElement.children[5]
+    if (url.value.length < 1 ) {        
+        tagError.innerHTML = "Url inválida, verique os dados!!"
+    } else {
+        tagError.innerHTML = ""
+    }  
+}
 
 function adicionarDadosNaPagina() {
 
   enviarCard.addEventListener("click", (enviar) => {
     enviar.preventDefault();
 
-   // if (tituloCard.value == ""){      
-    //  tagError.innerText = "O campo não pode estar vazio"
-      
- // }
-        
-    tituloCard.onblur = () => {     
-    tituloCard.value = tituloCard.value.toLocaleUpperCase()
-    
-    if (tituloCard.value === "") {        
-        tagError.innerHTML = "O mínimo de caracteres é 11"
-    } else {
-        tagError.innerHTML = ""
-    }   
-
-}
-    
-    
-
     let addCard = document.createElement("div");
     addCard.innerHTML = `
             <div class="card">
                     <div>
-                        <img class="background_card" src="${urlCard.value}" alt="">
+                        <img class="backgroundCard" src="${urlCard.value}" alt="">
                     </div>
-                    <h2 class="title_card">${tituloCard.value}</h2>
+                    <h2 class="titleCard">${tituloCard.value}</h2>
                     <p class="description">${descricaoCard.value}</p>
-                    <p class="categoriaCard">${categoriaCard.value}</p>
+                    <p class="categoriaCard">Categoria: ${categoriaCard.value}</p>
                 </div>
-            `;
-    document.querySelector(".dadosGaleria").appendChild(addCard);
-  });
+            `
+    document.querySelector(".dadosGaleria").appendChild(addCard)
+  })
 }
 
